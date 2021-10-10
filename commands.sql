a8122215032a00a202bd5df76c6e0d803491ab6e
YuriLukashenko <yuri.lukashenko@computools.com>
2021-10-10 11:46:58 +0300

    added ewer2020 bets2020 views, cases and payouts flow

--- commands.sql
+++ commands.sql
@@ -50,3 +50,153 @@ SELECT ta.se_id                   AS zz_id,
        zz.sum * ta.ex_rate * 0.05 AS tax
 from dbo.taxes as ta
 inner join dbo.zz zz on zz.id = ta.se_id
+
+
+create view dbo.e_2020(name, month, inuah) as
+SELECT ct.name,
+       eh.month,
+       eh.value * eh.rate AS inuah
+FROM dbo.ewer_history eh
+         JOIN dbo.currency_types ct ON eh.ct_id = ct.ct_id
+WHERE eh.year = 2020;
+
+create view dbo.e_2020_usd(total_usd) as
+SELECT sum(eh.value) AS total_usd
+FROM dbo.ewer_history eh
+WHERE eh.year = 2020
+  AND eh.ct_id = 1;
+
+create view dbo.e_2020_eur(total_eur) as
+SELECT sum(eh.value) AS total_eur
+FROM dbo.ewer_history eh
+WHERE eh.year = 2020
+  AND eh.ct_id = 2;
+
+create view dbo.e_2020_pln(total_pln) as
+SELECT sum(eh.value) AS total_pln
+FROM dbo.ewer_history eh
+WHERE eh.year = 2020
+  AND eh.ct_id = 3;
+
+create view dbo.e_2020_uah(total_uah) as
+SELECT sum(eh.value) AS total_uah
+FROM dbo.ewer_history eh
+WHERE eh.year = 2020
+  AND eh.ct_id = 4;
+
+create view dbo.e_2020_uah_bets(sum) as
+SELECT sum(eb.value) AS sum
+FROM (SELECT e.total_uah AS value
+      FROM dbo.e_2020_uah e
+      UNION ALL
+      SELECT b.without_cashback AS value
+      FROM dbo.b_2020_without_cashback b) eb;
+
+create view dbo.b_2020(bh_id, bet, outcome, commission, bet_date, outcome_date, comment) as
+SELECT bh.bh_id,
+       bh.bet,
+       bh.outcome,
+       bh.commission,
+       bh.bet_date,
+       bh.outcome_date,
+       bh.comment
+FROM dbo.bets_history bh
+WHERE bh.bet_date >= '2020-01-01'::date
+  AND bh.bet_date <= '2020-12-31'::date;
+
+create view dbo.b_2020_win_lose(win_lose) as
+SELECT sum(b2020.outcome) - sum(b2020.bet) AS win_lose
+FROM dbo.b_2020 b2020;
+
+create view dbo.b_2020_cashback(cashback) as
+SELECT sum(b2020.bet) * 0.03 AS cashback
+FROM dbo.b_2020 b2020
+WHERE b2020.bet_date > '2020-01-31'::date and (b2020.bh_id < 72 or b2020.bh_id > 97)
+
+create view dbo.b_2020_commission(total_commission) as
+SELECT sum(b2020.commission) AS total_commission
+FROM dbo.b_2020 b2020;
+
+create view dbo.b_2020_tax(tax) as
+SELECT b_2020_c.cashback * 0.195 AS tax
+FROM dbo.b_2020_cashback b_2020_c;
+
+create view dbo.b_2019_clean(clean) as
+SELECT bwl.win_lose + bca.cashback - bt.tax - bcm.total_commission
+from dbo.b_2019_win_lose as bwl,
+     dbo.b_2019_cashback as bca,
+     dbo.b_2019_tax as bt,
+     dbo.b_2019_commission as bcm
+
+create view dbo.b_2020_clean(clean) as
+SELECT bwl.win_lose + bca.cashback - bt.tax - bcm.total_commission
+from dbo.b_2020_win_lose as bwl,
+     dbo.b_2020_cashback as bca,
+     dbo.b_2020_tax as bt,
+     dbo.b_2020_commission as bcm;
+
+create view dbo.b_2020_without_cashback(without_cashback) as
+SELECT sum(b2020.outcome) - sum(b2020.bet) - sum(b2020.commission) AS without_cashback
+FROM dbo.b_2020 b2020;
+
+create view dbo.b_2020_debt_percent(percent) as
+SELECT bcl.clean / sum(b2020.outcome) * 100
+FROM dbo.b_2020 b2020, dbo.b_2020_clean bcl
+group by bcl.clean;
+
+create view dbo.b_2019_debt_percent(percent) as
+SELECT bcl.clean / sum(b2019.outcome) * 100
+FROM dbo.b_2019 b2019, dbo.b_2019_clean bcl
+group by bcl.clean;
+
+create table dbo.bets
+(
+    b_id        serial not null
+        constraint bets_pk
+            primary key,
+    bet          numeric,
+    outcome      numeric,
+    commission   numeric,
+    bet_date     date,
+    outcome_date date,
+    comment      varchar(256)
+);
+
+create view dbo.cases_view(id, usd, rate, inuah, date) as
+    select c.c_id as id,
+           c.usd,
+           c.rate,
+           (c.usd * c.rate) as inuah,
+           c.date from dbo.cases c;
+
+create view dbo.cases_total(total_usd, total_inuah, saved_usd)
+as select sum(cv.usd) as total_usd,
+          sum(cv.inuah) as total_inuah,
+          sum(cv.usd) - cpt.payed as saved_usd
+from dbo.cases_view cv, dbo.case_payouts_total cpt
+group by cpt.payed;
+
+create view dbo.case_payouts_view(id, usd, date, to_whom) as
+    select cp.cp_id as id,
+           cp.value_usd as usd,
+           cp.date,
+           cpp.name as to_whom
+   from dbo.case_payouts cp inner join dbo.case_payout_people cpp on cpp.cpp_id = cp.to_whom;
+
+create view dbo.case_payouts_by_person(name, initial, payed) as
+    select cpp.name,
+           cpi.value_usd as initial,
+           sum(cp.value_usd) as payed
+from dbo.case_payout_people cpp
+    inner join dbo.case_payout_initial cpi on cpp.cpp_id = cpi.cpp_id
+    inner join dbo.case_payouts cp on cpp.cpp_id = cp.to_whom
+group by cpp.name, cpi.value_usd;
+
+create view dbo.case_payouts_total(initial, payed) as
+    select sum(cpbp.initial) as initial,
+           sum(cpbp.payed) as payed
+    from dbo.case_payouts_by_person cpbp;
+
+create view dbo.case_payouts_left(value) as
+    select cpt.initial - cpt.payed
+    from dbo.case_payouts_total as cpt;
